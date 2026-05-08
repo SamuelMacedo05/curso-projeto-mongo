607dff02be0ca0a905988cc39e5cc4ebc4e134ff
Samuel Owen <owenmacedo05@gmail.com>
2026-05-08 10:59:52 -0300

    Referenciando os posts do usuário

--- src/main/java/com/samuel/workshopmongo/config/Instantiation.java
+++ src/main/java/com/samuel/workshopmongo/config/Instantiation.java
@@ -34,13 +34,16 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
 
         Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "vou viajar para São Paulo", new AuthorDTO( maria)  );
         Post p2 = new Post(null, sdf.parse("27/09/2012"), "Hello word", "vou viajar pelo mundo", new AuthorDTO(alex) );
 
         posRepository.saveAll(Arrays.asList(p1,p2));
+
+        maria.getPosts().addAll( Arrays.asList(p1,p2));
+        userRepository.save(maria);
 
     }
 }
--- src/main/java/com/samuel/workshopmongo/domain/User.java
+++ src/main/java/com/samuel/workshopmongo/domain/User.java
@@ -1,10 +1,13 @@
 package com.samuel.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,6 +20,11 @@ public class User implements Serializable {
     @Id
     String id;
     String email;
+
+    @DBRef (lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+
 
     public User() {
 
@@ -50,6 +58,13 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
